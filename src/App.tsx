69573d89479269468b575e4a62b88e8c05c9c160
Gerald-Waerseggers <r0853509@ucll.be>
2024-05-15 13:22:38 +0200

    Header gedaan

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-green-600">
-        Lekker van bij ons
-      </h1>
+      <img
+        src="https://www.lekkervanbijons.be/sites/default/files/styles/home_hero_xxlarge/public/images/HR_iStock_papa-kind-groenten.jpg?itok=8Hm8F_TU"
+        alt="groenten"
+        className="w-full absolute -z-10 object-cover brightness-90"
+      ></img>
+      <Header />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      centerBold: ['Center Bold Regular'],
+    },
     extend: {},
   },
   plugins: [],
