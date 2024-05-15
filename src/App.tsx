faf6b82c5ec34ef104559a0795fe30d896127943
Gerald-Waerseggers <r0853509@ucll.be>
2024-05-15 10:58:27 +0200

    Vite React Tailwindcss starter project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-green-600">
+        Lekker van bij ons
+      </h1>
+    </>
+  );
+}
+
+export default App;
