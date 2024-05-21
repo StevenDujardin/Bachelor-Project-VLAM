fe5981cfd96f13ff0d48296d3b3f6e1bdfd95361
StevenDujardin <steven.dujardin@student.ucll.be>
2024-05-21 14:15:28 +0200

    backend

---
+++ backend/app.ts
@@ -0,0 +1,16 @@
+// src/index.ts
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
