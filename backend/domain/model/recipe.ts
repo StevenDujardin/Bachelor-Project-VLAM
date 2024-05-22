7523a9a3a349a16649ce3b61dffafd59cc1ce4a6
StevenDujardin <steven.dujardin@student.ucll.be>
2024-05-22 14:08:48 +0200

    Backend zonder bugs

--- backend/domain/model/recipe.ts
+++ backend/domain/model/recipe.ts
@@ -2,7 +2,7 @@ export class Recipe {
     readonly recipe_id: number;
     readonly title: String;
     readonly description: String;
-    readonly steps: Array<String> = [];
+    readonly steps: Array<String>;
     readonly duration: String;
     readonly difficulty: String;
     readonly type: String;
@@ -22,6 +22,7 @@ export class Recipe {
         this.recipe_id = recipe.recipe_id;
         this.title = recipe.title;
         this.description = recipe.description;
+        this.steps = recipe.steps;
         this.duration = recipe.duration;
         this.difficulty = recipe.difficulty;
         this.type = recipe.type;
