6013987d13e9ed22a6d87dffeb50adeb2c42b246
StevenDujardin <steven.dujardin@student.ucll.be>
2024-05-23 10:20:01 +0200

    openAI API endpoint begin

--- backend/app.ts
+++ backend/app.ts
@@ -2,7 +2,12 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import * as bodyParser from "body-parser";
+<<<<<<< Updated upstream
 import { recipeRouter } from './src/controller/recipe.route';
+=======
+import { recipeRouter } from './controller/recipe.route';
+import { Configuration, OpenAIApi} from "openai"
+>>>>>>> Stashed changes
 
 const app = express();
 dotenv.config();
@@ -20,3 +25,14 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
+// OpenAI configuration creation
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_SECRET_KEY,
+});
+// OpenAI instance creation
+const openai = new OpenAIApi(configuration);
+
+app.get('/api', async (req, res) => {
+  let answer = openai.send(req.body)
+  res.send(answer);
+});
