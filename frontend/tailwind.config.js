b88e74315d26a1d421d9e3a1717776d4a86d2e0e
Gerald-Waerseggers <r0853509@ucll.be>
2024-05-27 11:39:31 +0200

    duration unit fix, fadein cards

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -32,6 +32,15 @@ export default {
         },
         LVBO: "#046a38",
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 1s ease-out",
+      },
     },
   },
   plugins: [
