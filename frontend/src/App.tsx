0690733f2556244d32b3efe4c69a2c474452a94d
Gerald-Waerseggers <r0853509@ucll.be>
2024-05-29 10:46:47 +0200

    error gefixed receptoverview + name refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
 import { OverOns } from "./pages/OverOns";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { ReceptenOveview } from "./pages/ReceptenOveview";
+import { ReceptenOverview } from "./pages/ReceptenOverview";
 import { Recept } from "./pages/Recept";
 import { BestPractice } from "./pages/BestPractice";
 import Login from "./pages/login";
@@ -18,7 +18,7 @@ function App() {
           <Route path="/login" element={<Login />}></Route>
           <Route path="/" element={<Home />}></Route>
           <Route path="/over_ons" element={<OverOns />}></Route>
-          <Route path="/recepten" element={<ReceptenOveview />}></Route>
+          <Route path="/recepten" element={<ReceptenOverview />}></Route>
           <Route path="/recepten/:recipe_id" element={<Recept />}></Route>
           <Route path="/best_practices" element={<BestPractice />}></Route>
 
