dd289ef1d7933b813ffc89f30442b30d2bd7acf2
Gerald-Waerseggers <r0853509@ucll.be>
2024-06-05 14:02:33 +0200

    fixje

--- frontend/src/provider/useAuth.ts
+++ frontend/src/provider/useAuth.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { AuthContext, AuthContextType } from "../provider/AuthProvider";
+import { AuthContext, AuthContextType } from "./AuthProvider";
 
 export const useAuth = (): AuthContextType => {
     const context = useContext(AuthContext);
