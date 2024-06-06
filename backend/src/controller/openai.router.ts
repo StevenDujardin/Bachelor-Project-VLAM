698503b82f01f6e7306b2a09938b48a55c1df1b8
JefLewandowski <78794308+JefLewandowski@users.noreply.github.com>
2024-06-06 13:41:06 +0200

    Implement dropdowns in backend

--- backend/src/controller/openai.router.ts
+++ backend/src/controller/openai.router.ts
@@ -7,7 +7,7 @@ const openAIRouter = express.Router();
 openAIRouter.post('/', async (req: Request, res: Response) => {
   try {
     console.log("generate")
-    const recipe = await recipeService.generateRecipe(req.body.message);
+    const recipe = await recipeService.generateRecipe(req.body.message, req.body.type, req.body.difficulty, req.body.duration);
     res.status(200).json(recipe);
   } catch (error: Error | any) {
     res.status(500).json({ status: error.message});
